e9ebdc2fd2f309ab438fee3e447acbe331c6241f
OwlVi <mrowlvi@gmail.com>
2025-12-21 03:43:55 +0700

    DB Init

---
+++ app/schemas/user.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+from typing import List
+
+class UserBase(BaseModel):
+    username: str
+    password: str
+    name: str
+
+class UserResponse(BaseModel):
+    username: str
+    name: str
+    email_accounts: List[EmailAccountResponse] = []
+
+    class Config:
+        from_attributes = True
